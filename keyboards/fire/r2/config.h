6394aff56f42f00d8a2cd710b62da2df28ebf500
QlDoors <ql.doors@gmail.com>
2024-04-10 18:46:08 +0800

    The code compiles through.

---
+++ keyboards/fire/r2/config.h
@@ -0,0 +1,83 @@
+#pragma once
+
+/* key matrix size */
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 15
+#define MATRIX_ROW_PINS { C7, C6, B6, B5, D0}
+#define MATRIX_COL_PINS { D2, D3, D5, D4, D6, D7, B4, F7, F6, F5, F4, F1, F0, B0, B1}
+
+//#define VIAL_KEYBOARD_UID {0xCA, 0xD8, 0xDE, 0x57, 0xCE, 0xA2, 0x74, 0xB7}
+
+/* COL2ROW, ROW2COL*/
+#define DIODE_DIRECTION COL2ROW
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3
+
+#define DEBOUNCE 5
+
+/* RGB matrix key backlighting */
+#define WS2812_DI_PIN D1
+#define RGB_MATRIX_LED_COUNT 75
+#define RGB_MATRIX_TIMEOUT 0 // number of milliseconds to wait until rgb automatically turns off
+#define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
+#define RGB_MATRIX_LED_PROCESS_LIMIT 75 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
+#define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 200 // limits maximum brightness of LEDs to 200 out of 255. If not defined maximum brightness is set to 255
+#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_CYCLE_ALL // Sets the default mode, if none has been set
+#define RGB_MATRIX_DEFAULT_HUE 0 // Sets the default hue value, if none has been set
+#define RGB_MATRIX_DEFAULT_SAT 255 // Sets the default saturation value, if none has been set
+#define RGB_MATRIX_DEFAULT_VAL 200 // Sets the default brightness value, if none has been set
+#define RGB_MATRIX_DEFAULT_SPD 170 // Sets the default animation speed, if none has been set
+#define RGB_TRIGGER_ON_KEYDOWN      // Triggers RGB keypress events on key down. This makes RGB control feel more responsive. This may cause RGB to not function properly on some boards
+#define RGB_MATRIX_CENTER { 112, 32 }
+
+// #define ENABLE_RGB_MATRIX_ALPHAS_MODS
+// #define ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+// #define ENABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
+#define ENABLE_RGB_MATRIX_BREATHING
+#define ENABLE_RGB_MATRIX_BAND_SAT
+#define ENABLE_RGB_MATRIX_BAND_VAL
+#define ENABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
+#define ENABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
+#define ENABLE_RGB_MATRIX_BAND_SPIRAL_SAT
+#define ENABLE_RGB_MATRIX_BAND_SPIRAL_VAL
+#define ENABLE_RGB_MATRIX_CYCLE_ALL
+#define ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+#define ENABLE_RGB_MATRIX_CYCLE_UP_DOWN
+#define ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+#define ENABLE_RGB_MATRIX_CYCLE_OUT_IN
+#define ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
+#define ENABLE_RGB_MATRIX_CYCLE_PINWHEEL
+#define ENABLE_RGB_MATRIX_CYCLE_SPIRAL
+#define ENABLE_RGB_MATRIX_DUAL_BEACON
+#define ENABLE_RGB_MATRIX_RAINBOW_BEACON
+#define ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS
+#define ENABLE_RGB_MATRIX_RAINDROPS
+#define ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+#define ENABLE_RGB_MATRIX_HUE_BREATHING
+#define ENABLE_RGB_MATRIX_HUE_PENDULUM
+#define ENABLE_RGB_MATRIX_HUE_WAVE
+#define ENABLE_RGB_MATRIX_PIXEL_RAIN
+// #define ENABLE_RGB_MATRIX_PIXEL_FLOW
+// #define ENABLE_RGB_MATRIX_PIXEL_FRACTAL
+// enabled only if RGB_MATRIX_FRAMEBUFFER_EFFECTS is defined
+// #define ENABLE_RGB_MATRIX_TYPING_HEATMAP
+// #define ENABLE_RGB_MATRIX_DIGITAL_RAIN
+// enabled only of RGB_MATRIX_KEYPRESSES or RGB_MATRIX_KEYRELEASES is defined
+// #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+// #define ENABLE_RGB_MATRIX_SOLID_REACTIVE
+// #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+// #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+// #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+// #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
+// #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
+// #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
+// #define ENABLE_RGB_MATRIX_SPLASH
+// #define ENABLE_RGB_MATRIX_MULTISPLASH
+// #define ENABLE_RGB_MATRIX_SOLID_SPLASH
+// #define ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
+
+// #define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_KEYPRESSES
+
+// #define RGB_MATRIX_SOLID_REACTIVE_GRADIENT_MODE
---
+++ keyboards/fire/r2/r2.c
@@ -0,0 +1,147 @@
+#include "r2.h"
+#include "rgb_matrix_types.h"
+
+led_config_t g_led_config = {{
+                                {10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24}, 
+                                {NO_LED, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25}, 
+                                {9, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, NO_LED, 51}, 
+                                {8, 65, 64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52}, 
+                                {66, 67, 68, 69, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, 70, 71, 72, 73, 74}
+                            },
+                             {{4, 47}, {4, 45}, {4, 44}, {4, 43}, {4, 41}, {4, 40}, {4, 38}, {4, 37}, {5, 28}, {8, 18}, {11, 8}, {26, 9}, {38, 9}, {50, 8}, {62, 10}, {74, 12}, {86, 14}, {98, 16}, {118, 15}, {130, 13}, {142, 11}, {154, 9}, {167, 8}, {179, 9}, {197, 9}, {203, 19}, {188, 19}, {175, 19}, {163, 18}, {151, 20}, {139, 22}, {127, 24}, {115, 26}, {90, 25}, {78, 23}, {66, 21}, {54, 19}, {41, 19}, {26, 19}, {21, 29}, {42, 29}, {54, 29}, {66, 31}, {78, 34}, {90, 36}, {120, 36}, {132, 34}, {144, 31}, {156, 29}, {169, 29}, {181, 29}, {201, 29}, {207, 40}, {190, 40}, {178, 40}, {165, 40}, {153, 40}, {141, 42}, {129, 45}, {117, 47}, {94, 47}, {82, 45}, {70, 42}, {58, 40}, {45, 40}, {25, 40}, {19, 50}, {35, 50}, {62, 52}, {83, 56}, {130, 55}, {154, 51}, {178, 50}, {190, 50}, {202, 50}},
+                             {2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4, 4, 4, 4, 4}};
+typedef union {
+    uint32_t raw;
+    struct {
+        bool rgb_abc;
+        bool rgb_line;
+        bool rgb_back;
+        bool rgb_line07;
+        int  rgb_line255;
+    };
+} user_config_t;
+
+user_config_t user_config;
+
+void          keyboard_post_init_user(void) {
+    user_config.raw = eeconfig_read_user();
+}
+
+void eeconfig_init_user(void) {
+    user_config.raw         = 0;
+    user_config.rgb_abc     = false;
+    user_config.rgb_line    = false;
+    user_config.rgb_back    = false;
+    user_config.rgb_line07  = false;
+    user_config.rgb_line255 = 0;
+    eeconfig_update_user(user_config.raw);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case KC_F19: // ——————————全灭
+            if (record->event.pressed) {
+                user_config.rgb_abc    = true;
+                user_config.rgb_line   = true;
+                user_config.rgb_back   = true;
+                user_config.rgb_line07 = true;
+                eeconfig_update_user(user_config.raw);
+            }
+            return true;
+        case KC_F20://——————————轴灯
+            if (record->event.pressed) {
+                if (user_config.rgb_abc) {
+                    user_config.rgb_abc = false;
+                    eeconfig_update_user(user_config.raw);
+                } else {
+                    user_config.rgb_abc = true;
+                    eeconfig_update_user(user_config.raw);
+                }
+            }
+            return true;
+        case KC_F21://——————————静止
+            if (record->event.pressed) {
+                if (user_config.rgb_back) {
+                    user_config.rgb_back = false;
+                    eeconfig_update_user(user_config.raw);
+                } else {
+                    user_config.rgb_back = true;
+                    eeconfig_update_user(user_config.raw);
+                }
+            }
+            return true;
+        case KC_F22://——————————条灯
+            if (record->event.pressed) {
+                if (user_config.rgb_line) {
+                    user_config.rgb_line = false;
+                    eeconfig_update_user(user_config.raw);
+                } else {
+                    user_config.rgb_line = true;
+                    eeconfig_update_user(user_config.raw);
+                }
+            }
+            return true;
+        case KC_F23://——————————彩虹
+            if (record->event.pressed) {
+                if (user_config.rgb_line07) {
+                    user_config.rgb_line07 = false;
+                    eeconfig_update_user(user_config.raw);
+                } else {
+                    user_config.rgb_line07 = true;
+                    eeconfig_update_user(user_config.raw);
+                }
+            }
+            return true;
+        case KC_F24://——————————彩虹变化
+            if (record->event.pressed) {
+                if (user_config.rgb_line255 > 190) {
+                    user_config.rgb_line255 -= 190;
+                    eeconfig_update_user(user_config.raw);
+                } else {
+                    user_config.rgb_line255 += 7;
+                    eeconfig_update_user(user_config.raw);
+                }
+            }
+            return true;
+        default:
+            return true;
+    }
+}
+
+bool rgb_matrix_indicators_kb(void) {
+    if (!rgb_matrix_indicators_user()) {
+        return false;
+    }
+    if (host_keyboard_led_state().caps_lock) {
+        rgb_matrix_set_color(39, 0, 255, 0);
+    }
+
+    if (user_config.rgb_abc) { // ————————————轴座开关
+        for (uint8_t i = 8; i < 75; ++i) {
+            if (i == 39 && host_keyboard_led_state().caps_lock) {
+                rgb_matrix_set_color(39, 0, 255, 0);
+            } else {
+                rgb_matrix_set_color(i, 0, 0, 0);
+            }
+        }
+    }
+    if (user_config.rgb_back) { // ————————————底灯开关
+        for (uint8_t i = 75; i < 118; ++i) {
+            rgb_matrix_set_color(i, 0, 0, 0);
+        }
+    }
+    if (user_config.rgb_line) { // ————————————灯条开关
+        for (uint8_t i = 0; i < 8; ++i) {
+            rgb_matrix_set_color(i, 0, 0, 0);
+        }
+    } else {
+        if (user_config.rgb_line07) { // ————————————彩虹开关
+            for (uint8_t i = 0; i < 8; ++i) {
+                HSV hsv = {user_config.rgb_line255 + i * 10, 255, rgb_matrix_get_val()};//10
+                RGB rgb = hsv_to_rgb(hsv);
+                rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
+            }
+        }
+    }
+    return true;
+}
